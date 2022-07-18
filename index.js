3c29054def66fab0fdf9fcdc9681d821e1d11b81
Justus B <justus.c.brown@gmail.com>
2022-07-18 10:52:05 -0700

    simple readme gen

--- index.js
+++ index.js
@@ -1,64 +1,113 @@
 // TODO: Include packages needed for this application
-
-const genMark = require("./utils/generateMarkdown.js")
-const inquirer = require("inquirer")
+const inquirer = require('inquirer');
+const fs = require('fs')
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input', 
+        type: 'input',
         message: 'Please enter the title for your project.',
-        name: 'Title',
+        name: 'title',
     },
     {
-        type: 'input', 
+        type: 'input',
         message: 'Please input a short description for your project.',
         name: 'desc',
     },
     {
-        type: 'input', 
+        type: 'input',
         message: 'Please enter any installation instructions for your project.',
         name: 'installation',
     },
     {
-        type: 'input', 
+        type: 'input',
         message: 'Please enter any usage instructions for your project.',
         name: 'usage',
     },
     {
-        type: 'input', 
+        type: 'input',
         message: 'Please enter any contribution guidelines for your project.',
-        name: 'cont-guide',
+        name: 'contrib',
     },
     {
-        type: 'input', 
+        type: 'input',
         message: 'Please enter any test instructions for your project.',
-        name: 'test-inst',
+        name: 'test',
     },
     {
-        type: 'input', 
-        message: 'Please enter any test instructions for your project.',
-        name: 'test-inst',
-    },
-    {
-        type: 'input', 
+        type: 'input',
         message: 'Please enter the license you would like to use for your project.',
         name: 'license',
     },
-    // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-    // description, installation instructions, usage information, contribution guidelines, and test instructions
+    {
+        type: 'input',
+        message: 'Please enter your github username.',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email',
+        name: 'email',
+    },
 ];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(
         questions
-    )
+    ).then(write)
 }
 
 // Function call to initialize app
 init();
+
+function write(data) {
+    fs.writeFile('readedadfasdf.md',
+ `# ${data.title}
+
+  ## Description
+
+  ${data.desc}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  
+  ## Installation
+
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+  
+  ## Badges
+  
+  ## How to Contribute
+
+  ${data.contrib}
+  
+  ## Tests
+
+  ${data.test}
+
+  ## Questions 
+
+  Github: [${data.github}](${data.github})
+  
+  Email: [${data.email}](${data.email})`
+
+        , function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Yay!')
+            }
+        })
+
+}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,54 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+
+  ${data.desc}
+  
+  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+  
+  - What was your motivation?
+  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+  - What problem does it solve?
+  - What did you learn?
+  
+  ## Table of Contents
+  
+  If your README is long, add a table of contents to make it easy for users to find what they need.
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+  
+  ## Badges
+  
+  ## How to Contribute
+
+  ${data.contrib}
+  
+  ## Tests
+
+  ${data.test}
+
+  ## Questions 
+
+  Github: ${data.github}
+  Email: ${data.email}
+  
 `;
 }
 
