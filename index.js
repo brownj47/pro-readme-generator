e72cce310706f1660043c82d1cc14152eba32678
Justus B <justus.c.brown@gmail.com>
2022-07-18 09:28:36 -0700

    require packages

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TODO: Include packages needed for this application
 
+const genMark = require("./utils/generateMarkdown.js")
+const inquirer = require("inquirer")
+
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
